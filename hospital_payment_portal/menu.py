e8eb3e3c1d1b31780318d0347e25e7d332c23ad3
VIPERJOHN6 <132152142+VIPERJOHN6@users.noreply.github.com>
2025-08-19 15:21:17 +0100

    forth commit

--- hospital_payment_portal/menu.py
+++ hospital_payment_portal/menu.py
@@ -1,6 +1,55 @@
-def menu():
-    while True:
-        print("\n  hospital payment portal  ")
-        print("1. login")
-        print("2. Register")
-        print("3. Exit")
+class Hospital():
+
+    def __init__(self, name, location):
+            self.name = name
+            self.location = location
+            print("\n Welcome to Bowen")
+    
+    def register(self):
+        gmail = input("Email: ")
+        full_name = input("Username: ")
+        password = input("Password: ")
+        verify_password = input("Confirm password: ")
+
+        if any(c.isupper() for c in password) and any(c.islower() for c in password) and any(c.isdigit() for c in password):
+            print("Successfully created an account")
+        else:
+            print("Wrong password or passwords do not match.")
+
+
+    def login(self):
+        Email = input("Enter your email: ")
+        password = input("Enter your Password")
+        if Email.islower() and any(c.isalpha() for c in Email) and any(c.isdigit() for c in Email):
+            print(Email)
+        else:
+            print("Incorrect email format.")
+        
+        if any(c.isupper() for c in password) and any(c.islower() for c in password) and any(c.isdigit() for c in password):
+            print("Successfully created an account")
+        else:
+            print("Wrong password or passwords do not match.")
+
+        
+    
+    def menu(self):
+        while True:
+            print("\n choose an option")
+            print("\n 1.Login")
+            print("\n 2.Register an account")
+
+            option = input("Enter an option (1-2): ")
+
+            if option == "1":
+                print(">>>>>>>> BOWEN< <<<<<")
+                self.login()
+            elif option == "2":
+                print(">>>>>>>> BOWEN< <<<<<")
+                self.register()
+            
+            break
+
+hospital = Hospital("Bowen Hospital", "Iwo")
+hospital.menu()
+
+
